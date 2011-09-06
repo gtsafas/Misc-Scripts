c19ac9738e88313a1e6b8b37724eeebd69f41fd1
George Tsafas <gtsafas@rblt.com>
2011-09-06 19:41:33 -0400

    Updated Symbology guess logic

--- perl/Symbology/Convention/NASDAQ/Integrated.pm
+++ perl/Symbology/Convention/NASDAQ/Integrated.pm
@@ -3,104 +3,296 @@ package Symbology::Convention::NASDAQ::Integrated;
 use strict;
 use warnings;
 
+# Enjoying Mastering regular expressions, why not use an example from it?
+my $template = {
+    'Preferred' => '-',
+    'Preferred Class' => '-=CLASS=',
+    'Class' => '.=CLASS=',
+    'Preferred when distributed' => '-$',
+    'When distributed' => '$',
+    'Warrants' => '+',
+    'Warrants Class' => '+=CLASS=',
+    'Called' => '*',
+    'Class Called' => '.=CLASS=*',
+    'Preferred Called' => '-*',
+    'Preferred Class Called' => '-=CLASS=*',
+    'Preferred Class When Issued' => '-=CLASS=#',
+    'Emerging Company Marketplace' => '!',
+    'Partial Paid' => '@',
+    'Convertible' => '%',
+    'Convertible Called' => '%*',
+    'Class Convertible' => '.=CLASS=%',
+    'Preferred Class Convertible' => '-=CLASS=%',
+    'Preferred Class When Distributed' => '-=CLASS=$',
+    'Rights' => '^',
+    'Units' => '=',
+    'When Issued' => '#',
+    'Rights When Issued' => '^#',
+    'Preferred When Issued' => '-#',
+    'Class When Issued' => '.=CLASS=#',
+    'Warrant When Issued' => '+#',
+    'TEST Symbol' => '~'
+};
+
+sub conversion {
+    my ($symbol,$type,$class) = @_;
+
+    $class = $class // '';
+    
+    my $suffix = $template->{$type};
+    die 'Unknown type: ' . $type unless defined $suffix;
+
+    $suffix =~ s/=CLASS=/$class/;
+
+    return $symbol . $suffix;
+
+}
+
+
 sub check {
     my ($self, $symbol) = @_;
 
     return unless defined $symbol;
 
-    return 'Preferred' 
-        if $symbol =~ m/^[A-Z]+-$/;
-
-    if ($symbol =~ m/^[A-Z]+-([A-Z])$/){
-        return "Preferred Class $1";
+    if ($symbol =~ m/^([A-Z]+)(-)$/){
+        return {
+            Type => 'Preferred',
+            Prefix => $1,
+            Suffix => $2,
+        }
     }
 
-    if ($symbol =~ m/^[A-Z]+\.([A-Z])$/) {
-        return "Class $1";
+    if ($symbol =~ m/^([A-Z]+)(-([A-Z]))$/){
+        return { 
+            Type => "Preferred Class",
+            Prefix => $1,
+            Suffix => $2,
+            Class => $3
+        }
     }
 
-    return 'Preferred when distributed'
-        if $symbol =~ m/^[A-Z]+-\$$/;
-
-    return 'When distributed'
-        if $symbol =~ m/^[A-Z]+\$$/;
-
-    return 'Warrants'
-        if $symbol =~ m/^[A-Z]+\+$/;
-
-    if ($symbol =~ m/^[A-Z]+\+([A-Z])$/){
-        return "Warrants Class $1";
+    if ($symbol =~ m/^([A-Z]+)(\.([A-Z]))$/) {
+        return { 
+            Type => "Class",
+            Prefix => $1,
+            Suffix => $2,
+            Class => $3
+        }
     }
 
-    return 'Called'
-        if $symbol =~ m/^[A-Z]+\*$/;
+    if ($symbol =~ m/^([A-Z]+)(-\$)$/){
+        return {
+            Type => 'Preferred when distributed',
+            Prefix => $1,
+            Suffix => $2,
+        }
+    }
+
+    if ($symbol =~ m/^([A-Z]+)(\$)$/){
+        return {
+            Type => 'When distributed',
+            Prefix => $1,
+            Suffix => $2,
+        }
+    }
+
+    if ($symbol =~ m/^([A-Z]+)(\+)$/){
+        return {
+            Type => 'Warrants',
+            Prefix => $1,
+            Suffix => $2
+        }
+    }
+
+    if ($symbol =~ m/^([A-Z]+)(\+([A-Z]))$/){
+        return { 
+            Type => "Warrants Class",
+            Prefix => $1,
+            Suffix => $2,
+            Class => $3
+        }
+    }
+
+    if ($symbol =~ m/^([A-Z]+)(\*)$/){
+        return {
+            Type => 'Called',
+            Prefix => $1,
+            Suffix => $2
+        }
+    }
     
-    if ($symbol =~ m/^[A-Z]+\.([A-Z])\*$/){
-        return "Class $1 Called";
+    if ($symbol =~ m/^([A-Z]+)(\.([A-Z])\*)$/){
+        return { 
+            Type => "Class Called",
+            Prefix => $1,
+            Suffix => $2,
+            Class => $3
+        }
     }
 
-    return 'Preferred Called'
-        if $symbol =~ m/^[A-Z]+-\*$/;
-
-    if ($symbol =~ m/^[A-Z]+-([A-Z])\*$/){
-        return "Preferred $1 Called";
+    if ($symbol =~ m/^([A-Z]+)(-\*)$/){
+        return {
+            Type => 'Preferred Called',
+            Prefix => $1,
+            Suffix => $2,
+        }
     }
 
-    if ($symbol =~ m/^[A-Z]+-([A-Z])#$/){
-        return "Preferred $1 When Issued";
+    if ($symbol =~ m/^([A-Z]+)(-([A-Z])\*)$/){
+        return { 
+            Type => "Preferred Class Called",
+            Prefix => $1,
+            Suffix => $2,
+            Class => $3
+        }
     }
 
-    return 'Emerging Company Marketplace'
-        if $symbol =~ m/^[A-Z]+!$/;
-
-    return 'Partial Paid' 
-        if $symbol =~ m/^[A-Z]+\@$/;
-
-    return 'Convertible' 
-        if $symbol =~ m/^[A-Z]+\%$/;
-
-    return 'Convertible Called' 
-        if $symbol =~ m/^[A-Z]+\%\*$/;
-
-    if ($symbol =~ m/^[A-Z]+\.([A-Z])\%$/){
-        return "Class $1 Convertible";
+    if ($symbol =~ m/^([A-Z]+)(-([A-Z])#)$/){
+        return { 
+            Type => "Preferred When Issued",
+            Prefix => $1,
+            Suffix => $2,
+            Class => $3
+        }
     }
 
-    if ($symbol =~ m/^[A-Z]+-([A-Z])\%$/){
-        return "Preferred Class $1 Convertible";
+    if ($symbol =~ m/^([A-Z]+)(!)$/){
+        return {
+            Type => 'Emerging Company Marketplace',
+            Prefix => $1,
+            Suffix => $2,
+        }
     }
 
-    if ($symbol =~ m/^[A-Z]+-([A-Z])\$$/){
-        return "Preferred Class $1 When Distributed";
+    if ($symbol =~ m/^([A-Z]+)(\@)$/){
+        return {
+            Type => 'Partial Paid',
+            Prefix => $1,
+            Suffix => $2,
+        }
     }
 
-    return 'Rights'
-        if $symbol =~ m/^[A-Z]+\^$/;
-
-    return 'Units'
-        if $symbol =~ m/^[A-Z]+=$/;
-
-    return 'When Issued'
-        if $symbol =~ m/^[A-Z]+#$/;
-
-    return 'Rights When Issued'
-        if $symbol =~ m/^[A-Z]+\^#$/;
-
-    return 'Preferred When Issued'
-        if $symbol =~ m/^[A-Z]+-#$/;
-
-    if ($symbol =~ m/^[A-Z]+\.([A-Z])#$/){
-        return "Class $1 When Issued";
+    if ($symbol =~ m/^([A-Z]+)(\%)$/){
+        return {
+            Type => 'Convertible',
+            Prefix => $1,
+            Suffix => $2,
+        }
     }
 
-    return 'Warrant When Issued'
-        if $symbol =~ m/^[A-Z]+\+#$/;
+    if ($symbol =~ m/^([A-Z]+)(\%\*)$/){
+        return {
+            Type => 'Convertible Called',
+            Prefix => $1,
+            Suffix => $2,
+        }
+    }
 
-    return 'TEST Symbol' 
-        if $symbol =~ m/^[A-Z]+~$/;
+    if ($symbol =~ m/^([A-Z]+)(\.([A-Z])\%)$/){
+        return { 
+            Type => "Class Convertible",
+            Prefix => $1,
+            Suffix => $2,
+            Class => $3
+        }
+    }
+
+    if ($symbol =~ m/^([A-Z]+)(-([A-Z])\%)$/){
+        return { 
+            Type => "Preferred Class Convertible",
+            Prefix => $1,
+            Suffix => $2,
+            Class => $3
+        }
+    }
+
+    if ($symbol =~ m/^([A-Z]+)(-([A-Z])\$)$/){
+        return { 
+            Type => "Preferred Class When Distributed",
+            Prefix => $1,
+            Suffix => $2,
+            Class => $3
+        }
+    }
+
+    if ($symbol =~ m/^([A-Z]+)(\^)$/){
+        return {
+            Type => 'Rights',
+            Prefix => $1,
+            Suffix => $2
+        }
+    }
+
+    if ($symbol =~ m/^([A-Z]+)(=)$/){
+        return {
+            Type => 'Units',
+            Prefix => $1,
+            Suffix => $2,
+        }
+    }
+
+    if ($symbol =~ m/^([A-Z]+)(#)$/){
+        return {
+            Type => 'When Issued',
+            Prefix => $1,
+            Suffix => $2
+        }
+    }
+
+    if ($symbol =~ m/^[A-Z]+\^#$/){
+        return {
+            Type => 'Rights When Issued',
+            Prefix => $1,
+            Suffix => $2,
+        }
+    }
+
+    if ($symbol =~ m/^([A-Z]+)(-#)$/){
+        return {
+            Type => 'Preferred When Issued',
+            Prefix => $1,
+            Suffix => $2,
+        }
+    }
+
+    if ($symbol =~ m/^([A-Z]+)(\.([A-Z])#)$/){
+        return { 
+            Type => "Class When Issued",
+            Prefix => $1,
+            Suffix => $2,
+            Class => $3
+        }
+    }
+
+    if ($symbol =~ m/^[A-Z]+\+#$/){
+        return {
+            Type => 'Warrant When Issued',
+            Prefix => $1,
+            Suffix => $2,
+        }
+    }
+
+    if ($symbol =~ m/^([A-Z]+)(~)$/){
+        return {
+            Type => 'TEST Symbol',
+            Prefix => $1,
+            Suffix => $2
+        }
+    }
 
     return undef;
 
 
 }
+
+sub giveme {
+    my ($self, $data) = @_;
+
+    if (exists $data->{Class}){
+        return conversion($data->{Prefix},$data->{Type},$data->{Class});
+    } else {
+        return conversion($data->{Prefix},$data->{Type});
+    }
+}
+
 1;
--- perl/Symbology/test.pl
+++ perl/Symbology/test.pl
@@ -9,5 +9,6 @@ use Converter;
 my $converter = Symbology::Converter->new();
 chomp($ARGV[0]);
 chomp($ARGV[1]);
-#print Dumper $converter->what($ARGV[0]);
-print Dumper $converter->convert($ARGV[0],$ARGV[1]);
+print Dumper $converter->what($ARGV[0]);
+
+#print Dumper $converter->convert($ARGV[0],$ARGV[1]);
